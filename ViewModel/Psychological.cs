882174d40434c334340b59b53a6a737d9fcb52a4
Logon Aluno <logonrmlocal@fiap.com.br>
2019-02-26 20:03:18 -0300

    initial commit

---
+++ ViewModel/Psychological.cs
@@ -0,0 +1,17 @@
+using System;
+using System.DateTime;
+ 
+namespace AN_Psychological.ViewModel
+{
+     
+        public class Psychological
+{
+
+public string Id{get; set;}
+public DateTime BirthDate{get;set;}
+public String Crp{get; set;}
+
+}
+
+     
+}
